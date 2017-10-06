0c38a5fbc30e5f8a68be42332ca25ee48ffccd56
Martin C <publiccouture@icloud.com>
2017-10-06 14:25:35 -0400

    Migration pour Python 3.x
    Migration pour Python 3.x

--- Assemblage_PDF.py
+++ Assemblage_PDF.py
@@ -8,7 +8,7 @@
 #
 # Created:     24/04/2014
 # Copyright:   (c) mcouture 2014
-# Licence:     ¿ utiliser ‡ vos propres risques.
+# Licence:     √Ä utiliser √† vos propres risques.
 # Infos Suppl: Vous devez installer la librairie PyPDF2 pour pouvoir l'utiliser.
 #              https://pypi.python.org/pypi/PyPDF2/1.15
 #-------------------------------------------------------------------------------
@@ -17,27 +17,27 @@
 import os, string, sys
 from PyPDF2 import PdfFileMerger, PdfFileReader
 
-#RÈpertoire o˘ se trouve les fichiers PDF.
+#R√©pertoire o√π se trouve les fichiers PDF.
 repertoire = sys.argv[1]
 listeFichiersPDF = os.listdir(repertoire) #Liste des fichiers
 
 
-CountPage = 0 #Nombre de pages totales mis ‡ 0
+CountPage = 0 #Nombre de pages totales mis √† 0
 
 merger = PdfFileMerger()
 
 for fichier in listeFichiersPDF:
-    if fichier.count(".") >0: #S'il y a des fichiers PDF dans le rÈpertoire.
+    if fichier.count(".") >0: #S'il y a des fichiers PDF dans le r√©pertoire.
         nbElementListe = fichier.split(".")
 
         if nbElementListe[len(nbElementListe)-1] =="pdf":
-            print "Assemblage : " + fichier
+            print("Assemblage : " + fichier)
             filename = repertoire+"\\"+fichier
-            merger.append(PdfFileReader(file(filename, 'rb'))) #Assemblage du PDF
-            CountPage = CountPage + PdfFileReader(file(filename, 'rb')).getNumPages() #Retourne le nombre de pages dans le fichier
+            merger.append(PdfFileReader(open(filename, 'rb'))) #Assemblage du PDF
+            CountPage = CountPage + PdfFileReader(open(filename, 'rb')).getNumPages() #Retourne le nombre de pages dans le fichier
 
-merger.write(repertoire+"\\PDFAssemble.pdf") #…criture du fichier PDF.
+merger.write(repertoire+"\\PDFAssemble.pdf") #√âcriture du fichier PDF.
 merger.close() #Fermeture du fichier
-#SynthËse
-print "Pages totaux assemblees = " + str(CountPage)
-print "Pages totaux du document = " + str(PdfFileReader(file(repertoire+"\\PDFAssemble.pdf", 'rb')).getNumPages())
+#Synth√®se
+print("Pages totaux assemblees = " + str(CountPage))
+print("Pages totaux du document = " + str(PdfFileReader(open(repertoire+"\\PDFAssemble.pdf", 'rb')).getNumPages()))
